9c94e478d614acfd2f9a498929ca546beb94506f
MVendrik <35732059+MVendrik@users.noreply.github.com>
2019-06-21 14:24:05 +0200

    Fix for compose enhancers.

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import thunk from "redux-thunk";
 
 const composeEnhancers =
   process.env.NODE_ENV === "development"
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
     : null || compose;
 
 const rootReducer = combineReducers({
